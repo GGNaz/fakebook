5f8044745e21ec9945d352d7ba806e6806a6398e
Nazer Somera <nazersomera123@gmail.com>
2023-01-17 08:28:29 +0800

    saved

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import './App.css';
 import Home from './Pages/Homepage/Home';
 import Navbar from './Components/Navbar/Navbar';
 import Chatroom from './Components/HomeComponents/Chatroom';
+import { Routes, Route } from "react-router-dom";
+import Login from './Pages/LoginPage/Login';
 
 function App() {
   return (
-    <div className='flex flex-col h-screen min-h-screen min-w-full p-2 bg-gray-100'>
-      <Navbar/>
-      <Home/>
-      <Chatroom/>
+    <div>
+      <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={
+        <div className='flex flex-col h-screen min-h-screen min-w-full p-2 bg-gray-100'>
+        <Navbar/>
+        <Home/>
+        <Chatroom/>
+      </div> 
+      }>
+      {/* <div className='flex flex-col h-screen min-h-screen min-w-full p-2 bg-gray-100'>
+        <Navbar/>
+        <Home/>
+        <Chatroom/>
+      </div> */}
+      </Route>
+      </Routes>
     </div>
   );
 }
