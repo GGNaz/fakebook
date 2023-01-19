79ce27ae5f04fd3929aa8994f78160896cc19f71
Nazer Somera <nazersomera123@gmail.com>
2023-01-19 16:06:49 +0800

    saved

--- src/Api/server_url.js
+++ src/Api/server_url.js
@@ -1,7 +1,7 @@
 // UAT
-const server = "https://ilagan.uat.api.xyphersolutionsinc.com/"
+// const server = "https://ilagan.uat.api.xyphersolutionsinc.com/"
 
 // Localhost
-// const server = "http://localhost:5090"
+const server = "http://localhost:5090"
 
 export default server
