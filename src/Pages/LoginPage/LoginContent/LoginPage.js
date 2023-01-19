9110ccbcf92bc1aa84c31be4853c4575642cd2d0
Ralph Levita <ralph.levita30@gmail.com>
2023-01-19 15:37:03 +0800

    fix

--- src/Pages/LoginPage/LoginContent/LoginPage.js
+++ src/Pages/LoginPage/LoginContent/LoginPage.js
@@ -70,31 +70,13 @@ function LoginPage() {
           Sign in
         </button>
       </div>
-      <div>
-        <button
-          type="button"
-          class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
-          data-mdb-ripple="true"
-          data-mdb-ripple-color="light"
-        >
-          <svg
-            class="w-4 h-4 mr-2 -ml-1"
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fab"
-            data-icon="google"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 488 512"
-          >
-            <path
-              fill="currentColor"
-              d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-            ></path>
-          </svg>
-          Sign in with Google
-        </button>
-      </div>
+      <p className="mt-8 text-xs font-light text-center text-gray-700">
+        {" "}
+        Don't have an account?{" "}
+        <a href="#" className="font-medium text-purple-600 hover:underline">
+          Sign up
+        </a>
+      </p>
     </div>
   );
 }
