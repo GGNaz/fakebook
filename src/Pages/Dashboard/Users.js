97f45fc794639b33f84ac16e32fcbb60aaae646b
Ralph Levita <ralph.levita30@gmail.com>
2023-01-25 16:34:34 +0800

    fix

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,45 +1,152 @@
-import React from 'react'
-import { Route, Routes } from 'react-router';
+import React from "react";
+import { Route, Routes } from "react-router";
 
-import Sidebar from '../../Components/DashboardComponents/Sidebar'
-import Navbar from '../../Components/Navbar/Navbar';
-import { getAllComponentsRoutes } from '../../middleware/getAllComponentsRoutes';
-import { dashboardList } from '../../routes';
+import Sidebar from "../../Components/DashboardComponents/Sidebar";
+import Navbar from "../../Components/Navbar/Navbar";
+import { getAllComponentsRoutes } from "../../middleware/getAllComponentsRoutes";
+import { dashboardList } from "../../routes";
 
 function Users() {
   return (
     <div className="flex flex-col h-screen w-full gap-2">
-    <Navbar title="Manage Users" desc="Manage user activities"/>
-    <div><input type="text" placeholder='search' /></div>
-    <table className="table table-auto rounded-lg w-full bg-white p-2">
-  <thead className='h-10 '>
-    <tr className='bg-skytheme p-1'>
-      <th className='text-white'>Username</th>
-      <th  className='text-white'>Post</th>
-      <th  className='text-white'>Comment</th>
-      <th  className='text-white'>Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr className='border-b'>
-      <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-      <td>Malcolm Lockyer</td>
-      <td>1961</td>
-    </tr>
-    <tr className='border-b'>
-      <td>Witchy Woman</td>
-      <td>The Eagles</td>
-      <td>1972</td>
-    </tr>
-    <tr>
-      <td>Shining Star</td>
-      <td>Earth, Wind, and Fire</td>
-      <td>1975</td>
-    </tr>
-  </tbody>
-</table>
+      <Navbar title="Manage Users" desc="Manage user activities" />
+      <div className="flex-row">
+        <div className="my-8 ">
+          <input type="text" placeholder="search" />
+        </div>
+        <div>
+          <button
+            type="button"
+            class="focus:outline-none text-white bg-red-700 hover:bg-red-400 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-500 dark:hover:bg-red-700 dark:focus:ring-red-900"
+          >
+            Invite New User
+          </button>
+        </div>
+      </div>
+
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50  dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                User Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  Permission
+                  <a href="#">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-3 h-3 ml-1"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      viewBox="0 0 320 512"
+                    >
+                      <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  2FA Auth
+                  <a href="#">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-3 h-3 ml-1"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      viewBox="0 0 320 512"
+                    >
+                      <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <div className="flex items-center">
+                  Last Active
+                  <a href="#">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-3 h-3 ml-1"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      viewBox="0 0 320 512"
+                    >
+                      <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                    </svg>
+                  </a>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <span className="sr-only">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b  dark:border-gray-200">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray"
+              >
+                Ralph Cutie
+              </th>
+              <td className="px-6 py-4">Admin</td>
+              <td className="px-6 py-4">Enabled</td>
+              <td className="px-6 py-4">2 mins</td>
+              <td className="px-6 py-4 text-right">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+            <tr className="bg-white border-b  dark:border-gray-200">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray"
+              >
+                Nazer Cutie
+              </th>
+              <td className="px-6 py-4">Admin</td>
+              <td className="px-6 py-4">Enabled</td>
+              <td className="px-6 py-4">5 mins</td>
+              <td className="px-6 py-4 text-right">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+            <tr className="bg-white ">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray"
+              >
+                Brian Cutie
+              </th>
+              <td className="px-6 py-4">Admin</td>
+              <td className="px-6 py-4">Enabled</td>
+              <td className="px-6 py-4">5 mins</td>
+              <td className="px-6 py-4 text-right">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Users
+export default Users;
