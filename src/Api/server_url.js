9e964933d99625ea9e4d6cd008e3df15bacc1090
Naz <93169309+GGNaz@users.noreply.github.com>
2023-03-30 08:53:44 +0800

    update server

--- src/Api/server_url.js
+++ src/Api/server_url.js
@@ -1,7 +1,7 @@
 // UAT
-// const server = "https://ilagan.uat.api.xyphersolutionsinc.com/"
+const server = "barker-server-hilitpqc9-briankarlsayen.vercel.app"
 
 // Localhost
-const server = "http://localhost:5095"
+// const server = "http://localhost:5095"
 
 export default server
