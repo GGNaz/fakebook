6fc521aa563130e2e83004fd3ad95b8dc3f6c7d5
Naz <93169309+GGNaz@users.noreply.github.com>
2023-04-14 16:11:01 +0800

    Update server_url.js

--- src/Api/server_url.js
+++ src/Api/server_url.js
@@ -1,5 +1,5 @@
 // UAT
-const server = "https://barker-server-hilitpqc9-briankarlsayen.vercel.app"
+const server = "https://twitty-server-p6yjbz8no-briankarlsayen.vercel.app/"
 
 // Localhost
 // const server = "http://localhost:5095"
